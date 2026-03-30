39649e335cdf835d7a8c0b44055d84cd1c2c1b3c
Faraz Safdar <fsafdar2@gmail.com>
2026-03-30 16:51:29 -0400

    Initial Commit

---
+++ COMP3097 Lab9/COMP3097_Lab9App.swift
@@ -0,0 +1,17 @@
+//
+//  COMP3097_Lab9App.swift
+//  COMP3097 Lab9
+//
+//  Created by Faraz on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct COMP3097_Lab9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
